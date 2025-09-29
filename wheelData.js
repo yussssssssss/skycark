5a3821a15575bea105a5b9cdc8035848ea97e253
yustyy <yusuf.acmaci@hotmail.com>
2025-09-29 23:20:54 +0300

    initial

---
+++ wheelData.js
@@ -0,0 +1,9 @@
+function getWheelData() {
+    return [
+      { value: " Sisal Çanta", color: "#db7093", percent: 10 }, // 50% şans
+      { value: "Redbull", color: "#20b2aa", percent: 15 },  // 10% şans
+      { value: "Kalem", color: "#ff340f", percent:30 },   // 10% şans
+      { value: "Kalem", color: "#ff340f", percent: 30 },    // 10% şans
+      { value: "Anahtarlık", color: "#4169e1", percent: 15 } // 20% şans
+    ];
+}
