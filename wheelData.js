8c966843cdaf1d4166418e1cc0897580aa2f5fee
yustyy <yusuf.acmaci@hotmail.com>
2025-10-01 10:53:31 +0300

    guncel

--- wheelData.js
+++ wheelData.js
@@ -1,10 +1,10 @@
 function getWheelData() {
     return [
       { value: "KALEM", color: "#db7093", percent: 15 }, 
-      { value: "SKYPACK", color: "#20b2aa", percent: 10 }, 
-      { value: "ANAHTARLIK", color: "#ff340f", percent:25 },  
+      { value: "SKYPACK", color: "#20b2aa", percent: 5 }, 
+      { value: "ANAHTARLIK", color: "#ff340f", percent:30 },  
        { value: "KALEM", color: "#db7093", percent: 15 }, 
-      { value: "SKYPACK", color: "#20b2aa", percent: 10 }, 
-      { value: "ANAHTARLIK", color: "#ff340f", percent:25 }
+      { value: "SKYPACK", color: "#20b2aa", percent: 5 }, 
+      { value: "ANAHTARLIK", color: "#ff340f", percent:30 }
     ];
 }
